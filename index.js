67b4f2fd3b122ccdf046792b9fb3040c41a31739
Visagan Shan <visagan.shan@gmail.com>
2023-06-08 03:21:03 -0400

    cors package added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 require("dotenv").config();
 const PORT = process.env.PORT || 5050;
 
+app.use(cors());
 const warehouseRoutes = require('./routes/warehouses-routes');
 app.use(express.json());
 // all users routes
